53c96a737c1b763f364f656af0a71a418d15b649
unknown <2185596822@qq.com>
2020-05-19 20:54:06 +0800

    tset

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         black_list
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       Another1024
-// @match        www.baidu.com
+// @match        *://*/*
 // ==/UserScript==
-
+var black_list = {"blog.csdn.net":'/<[^>]+>/gim'};
+var host;
 function do_strip(reg,s) {
     return s.replace(reg, '');
 }
 
 function main() {
-var reg=/<[^>]+>/gim;
-var the_html = document.getElementsByTagName("html")[0];
-the_html.innerText=do_strip(reg,the_html.innerText);
+
+    var the_html = document.getElementsByTagName("html")[0];
+    the_html.innerText=do_strip(black_list[host],the_html.innerText);
 }
 
 
 (function() {
     'use strict';
-    var int=self.setInterval(main(),1000);
-    // Your code here...
+    host = window.location.host;
+    console.log(host);
+    if(black_list[host]){
+        var int=self.setInterval(main(),1000);
+    }
+        // Your code here...
 })();
